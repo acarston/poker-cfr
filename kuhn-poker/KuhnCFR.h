ee8c19da0c247564f5f5a1922b89bae2594313ce
acarston <amcarston@gmail.com>
2022-12-22 18:45:13 -0700

    make necessary functions private

--- kuhn-poker/KuhnCFR.h
+++ kuhn-poker/KuhnCFR.h
@@ -11,18 +11,14 @@ class KuhnCFR {
     public:
         unordered_map<string, Node> nodes;
 
-        //change name to verb
-        //calculateRegrets, AccumulateRegrets, AddRegrets
         double CalculateUtilities(vector<int> cards, string history = "", vector<double> reachProbabilities = {1.0, 1.0});
-        Node& GetNode(string key); //both methods should be private
-        double GetTerminalUtility(string history, vector<int> cards, double nodeUtility, int currentPlayer);
     private:
         const int TERMINAL_HISTORY_LENGTH = 2, PLAYER_1 = 0, PLAYER_2 = 1;
         const vector<string> ACTIONS = {"b", "p"};
         const int NUM_ACTIONS = ACTIONS.size();
 
-        // double GetTerminalUtility(string history, vector<int> cards);
-        //Node GetNode(string key);
+        double GetTerminalUtility(string history, vector<int> cards, double nodeUtility, int currentPlayer);
+        Node& GetNode(string key);
 };
 
 #endif
