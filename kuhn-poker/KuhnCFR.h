635b2976ef860cb08b4418330c07b9ef5489b039
acarston <amcarston@gmail.com>
2022-12-22 21:34:17 -0700

    change scope of constants

--- kuhn-poker/KuhnCFR.h
+++ kuhn-poker/KuhnCFR.h
@@ -9,11 +9,12 @@ using namespace std;
 
 class KuhnCFR {
     public:
+        const int PLAYER_1 = 0, PLAYER_2 = 1;
         unordered_map<string, Node> nodes;
 
         double CalculateUtilities(vector<int> cards, string history = "", vector<double> reachProbabilities = {1.0, 1.0});
     private:
-        const int TERMINAL_HISTORY_LENGTH = 2, PLAYER_1 = 0, PLAYER_2 = 1;
+        const int TERMINAL_HISTORY_LENGTH = 2;
         const vector<string> ACTIONS = {"b", "p"};
         const int NUM_ACTIONS = ACTIONS.size();
 
