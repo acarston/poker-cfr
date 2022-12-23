b9372df150b84a0dc97ce45a9179eaa6cc6d0df5
acarston <amcarston@gmail.com>
2022-12-22 21:08:00 -0700

    write the main function

--- kuhn-poker/main.cpp
+++ kuhn-poker/main.cpp
@@ -1,5 +1,11 @@
 #include "Trainer.cpp"
 
 int main() {
+    Trainer trainer;
+    int numIterations = 100;
+
+    trainer.Train(numIterations);
+    trainer.DisplayNodeStrategies(numIterations);
+    
     return 0;
 };
