445094802363d04e195a726c867349f9a6ed3986
acarston <amcarston@gmail.com>
2022-12-22 19:03:26 -0700

    add comments

--- kuhn-poker/KuhnCFR.cpp
+++ kuhn-poker/KuhnCFR.cpp
@@ -1,15 +1,18 @@
 #include "KuhnCFR.h"
 
+/* pass forward reach probabilities and pass back game-state utilities; calculate and update regrets */
 double KuhnCFR::CalculateUtilities(vector<int> cards, string history, vector<double> reachProbabilities) {
     double nodeUtility = 0.0;
-    int currentPlayer = (history.size() % 2 == 0) ? PLAYER_1 : PLAYER_2;
+    int currentPlayer = (history.size() % 2 == 0) ? PLAYER_1 : PLAYER_2; 
 
+    // return payoff for terminal nodes
     if (history.size() > 1) nodeUtility = GetTerminalUtility(history, cards, nodeUtility, currentPlayer);
     if (nodeUtility != 0.0) return nodeUtility;
 
     string key = to_string(cards.at(currentPlayer)) + history;
     Node& currentNode = GetNode(key);
 
+    // compute current game-state and counterfactual utilities for each node in the game tree
     vector<double> nextReachProbabilities = reachProbabilities;
     vector<double> counterfactualUtilities(NUM_ACTIONS, 0.0);
     vector<double> nodeStrategy = currentNode.GetCurrentStrategy(reachProbabilities.at(currentPlayer));
@@ -20,6 +23,7 @@ double KuhnCFR::CalculateUtilities(vector<int> cards, string history, vector<dou
         nodeUtility += counterfactualUtilities.at(i) * nextReachProbabilities.at(currentPlayer);
     }
 
+    // update the node's cumulative regrets
     int opponent = !bool(currentPlayer);
     for (unsigned int i = 0; i < counterfactualUtilities.size(); ++i) {
         currentNode.cumulativeRegrets.at(i) += counterfactualUtilities.at(i) - nodeUtility;
@@ -29,6 +33,7 @@ double KuhnCFR::CalculateUtilities(vector<int> cards, string history, vector<dou
     return nodeUtility;
 };
 
+/* return payoff for current player according to the rules of Kuhn poker */
 double KuhnCFR::GetTerminalUtility(string history, vector<int> cards, double nodeUtility, int currentPlayer) {
     int terminalHistoryStart = history.size() - TERMINAL_HISTORY_LENGTH;
     string terminalHistory = history.substr(terminalHistoryStart, TERMINAL_HISTORY_LENGTH);
@@ -44,6 +49,7 @@ double KuhnCFR::GetTerminalUtility(string history, vector<int> cards, double nod
     return nodeUtility;
 };
 
+/* create new key-value pair if necessary and return node */
 Node& KuhnCFR::GetNode(string key) {
     unordered_map<string, Node>::iterator it = nodes.find(key);
 
